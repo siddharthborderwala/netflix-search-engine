e804a0ac45c5f524f2f763d69f5ecb41356dbe03
Siddharth Borderwala <siddharthborderwala@gmail.com>
2021-10-26 14:59:43 +0530

    create jinja templates

--- server/app.py
+++ server/app.py
@@ -1,8 +1,24 @@
 from flask.app import Flask
+from flask.templating import render_template
+from flask import request
+
+from lib.query import get_results
 
 app = Flask(__name__)
 
 
 @app.get("/")
 def root():
-    return "Hello, World!"
+    return render_template("home.html")
+
+
+@app.post("/query")
+@app.errorhandler(400)
+@app.errorhandler(404)
+def make_query():
+    try:
+        query = request.form['query']
+    except KeyError:
+        return "Invalid Query", 400
+    results = get_results(query)
+    return render_template("results.html", results=results)
