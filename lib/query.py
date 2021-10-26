60d74da8bf80b3254e885e9d7137c9332f5a9665
Siddharth Borderwala <siddharthborderwala@gmail.com>
2021-10-26 15:08:52 +0530

    lib module

---
+++ lib/query.py
@@ -0,0 +1,9 @@
+def get_results(query):
+    return [
+        {
+            "link": ".",
+            "title": "ASD",
+            "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum voluptates temporibus quasi eligendi perferendis nam, eos vitae nobis architecto dignissimos nesciunt natus nulla est quisquam, rerum qui. Harum, nesciunt quas!",
+            "date": "12th Feb, 2021"
+        }
+    ]
